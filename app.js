02c7e745563d7ab06379627c4f7eb3210066357f
Shane Upchurch <shaneupchurch@gmail.com>
2020-10-12 17:34:31 -0500

    sets DOM elements

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 // import functions and grab DOM elements
 
+const results = document.querySelector('#results');
+const radios = document.querySelectorAll ('input');
+const images = document.querySelectorAll('label > img');
+const resetButton = document.querySelector('button');
+
+console.log(results);
+console.log(radios);
+console.log(images);
+console.log(resetButton);
+
+
 // initialize state
 
 // set event listeners to update state and DOM
