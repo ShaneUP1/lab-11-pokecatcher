94f17bdd09c7802844a84e8b558fd5c19191bce2
Shane Upchurch <shaneupchurch@gmail.com>
2020-10-12 22:56:04 -0500

    final push of the evening

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const resetButton = document.querySelector('button');
 
 
 // initialize state
-let rounds = 0;
+let rounds = 10;
 let resultsArray = [];
 
 // set event listeners to update state and DOM
@@ -41,40 +41,21 @@ function generatePokemon(pokeData) {
     radios[2].value = poke3.pokemon;
     images[2].src = poke3.url_image;
 
-    // for (let i = 0; i < radios.length; i++) {
-    //     rounds++;
-
-    //     const pokeStored = {
-    //         pokemon: poke[i].pokemon,
-    //         encountered: 1,
-    //         captured: 1,
-    //     };
-
-    //     if (resultsArray.includes(pokeStored)) {
-    //         pokeStored.encountered++;
-    //     }
-    //     push.resultsArray(pokeStored);
 
     for (let i = 0; i < radios.length; i++) {
         radios[i].addEventListener('change', (e) => {
-            rounds++;
+            rounds--;
 
-            console.log(e.target.value);
             // create objects of each pokemon
 
-            // check to see if they already exist in our
-            // resultsArray and if so increase their encounters and potentially captures 
-
-            // push the object into our resultsArray
-        });
+            const pokeCapturedName = (e.target.value);
+            console.log(pokeCapturedName);
 
 
 
 
-
-
+        })
+        generatePokemon(pokeData);
 
     }
-}
-generatePokemon(pokeData);
 
