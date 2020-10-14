4c05643a58c594aa202a3cc223eeef5f49c10c8e
Shane Upchurch <shaneupchurch@gmail.com>
2020-10-14 13:59:29 -0500

    adds pie chart, still need some styling

--- results/results.js
+++ results/results.js
@@ -1,4 +1,5 @@
-var ctx = document.getElementById('myChart').getContext('2d');
+var ctx = document.getElementById('pokemonChart').getContext('2d');
+var cty = document.getElementById('pokeEButNotC').getContext('2d');
 
 import { getFromLocalStorage } from '../utils.js';
 
@@ -7,23 +8,29 @@ const newPokeArray = getFromLocalStorage('POKEMON') || [];
 
 
 
-const pokemonNumbers = [];
+const pokemonCaught = [];
+const pokemonSeen = [];
 const labels = [];
 
 for (let i = 0; i < newPokeArray.length; i++) {
     const singlePoke = newPokeArray[i];
 
-    pokemonNumbers.push(singlePoke.captured);
+    pokemonCaught.push(singlePoke.captured);
+    pokemonSeen.push(singlePoke.encountered);
     labels.push(singlePoke.name);
 }
 
+
 const colors = [
     'lavender',
-    'rgba(54, 162, 235, 0.2)',
-    'rgba(255, 206, 86, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(255, 159, 64, 0.2)'
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'indigo',
+    'blue',
+    'violet',
+
 ];
 const borders = [
     'orange',
@@ -33,16 +40,62 @@ const borders = [
     'rgba(153, 102, 255, 1)',
     'rgba(255, 159, 64, 1)'
 ];
-console.log(labels, pokemonNumbers);
+
 
 new Chart(ctx, {
-    type: 'horizontalBar',
+    type: 'bar',
     data: {
         // labels: labels,
         labels,
         datasets: [{
-            label: '# of Pokemo Caught',
-            data: pokemonNumbers,
+            label: 'Times Pokemon Seen',
+            data: pokemonSeen,
+            backgroundColor: 'lightblue',
+            borderColor: 'orange',
+            borderWidth: 3,
+        }, {
+            label: 'Times Pokemon Caught',
+            data: pokemonCaught,
+            backgroundColor: 'yellow',
+            borderColor: 'orange',
+            borderWidth: 3,
+        }
+        ]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
+const seenButNeverCaught = [];
+const seenThisManyTimes = [];
+console.log(seenButNeverCaught);
+console.log(seenThisManyTimes);
+
+for (let i = 0; i < newPokeArray.length; i++) {
+
+    const indPokemon = newPokeArray[i];
+    if (indPokemon.encountered !== 0 && indPokemon.captured === 0) {
+        seenButNeverCaught.push(indPokemon.name);
+        seenThisManyTimes.push(indPokemon.encountered);
+    }
+}
+
+new Chart(cty, {
+    type: 'pie',
+    data: {
+        // labels: labels,
+        seenButNeverCaught,
+        datasets: [{
+            label: 'Pokemon Seen But Never Caught',
+            data: seenThisManyTimes,
             backgroundColor: colors,
             borderColor: borders,
             borderWidth: 5
